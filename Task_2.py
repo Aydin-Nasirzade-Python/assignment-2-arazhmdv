f2c73a60352f13c12a5f1355bde112f175bacf76
arazhmdv <165569292+arazhmdv@users.noreply.github.com>
2024-04-07 17:05:57 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -3,13 +3,13 @@
 def main():
   month = input('Enter name of the month [ex. June]: ')
   day = int(input('Enter the day [ex. 5]: '))
-  if (month == 'March' and day > 20) or month == 'April' or month == 'May' or (month == 'June' and day < 21):
+  if (month == 'March' and day >= 20) or month == 'April' or month == 'May' or (month == 'June' and day < 21):
       print(month,day, 'is in Spring')
   elif (month == 'June' and day >= 21) or month == 'July' or month == 'August' or (month == 'September' and day <22):
       print(month, day, 'is in Summer')
   elif (month == 'September' and day >= 22) or month == 'October' or month == 'November' or (month == 'December' and day <21):
       print(month, day, 'is in Fall')
-  elif (month == 'December' and day >= 21) or month == 'January' or month == 'February' or (month == 'March' and day <22):
+  elif (month == 'December' and day >= 21) or month == 'January' or month == 'February' or (month == 'March' and day <20):
       print(month, day, 'is in Winter')
 
   pass
