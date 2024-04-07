93cb2c7bfc9ebbfcbf535d35a056a992e5036bc5
arazhmdv <165569292+arazhmdv@users.noreply.github.com>
2024-04-07 16:48:46 +0400

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -1,9 +1,25 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  a = float(input("Enter the wavelength in nm: "))
+  
+  if 380 <= a < 450:
+      b = "Violet"
+  elif 450 <= a < 495:
+      b = "Blue"
+  elif 495 <= a < 570:
+      b = "Green"
+  elif 570 <= a < 590:
+      b = "Yellow"
+  elif 590 <= a < 620:
+      b = "Orange"
+  elif 620 <= a <= 750:
+      b = "Red"
+  else:
+      b = "Invalid input!"
+  
+  print(f"The relevant color is {b}")
+
   pass
 
 if __name__ == "__main__":
